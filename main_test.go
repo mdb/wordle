9949f333d0a79cd923fe33ae23c94ed58fdca471
Mike Ball <mikedball@gmail.com>
2022-02-07 11:29:41 -0500

    enable proiding multiple inputs per test case

--- main_test.go
+++ main_test.go
@@ -10,15 +10,15 @@ import (
 func TestRun(t *testing.T) {
 	tests := []struct {
 		word   string
-		input  string
+		inputs []string
 		output string
 	}{{
 		word:   "BEACH",
-		input:  "see",
+		inputs: []string{"see"},
 		output: "SEE is not a 5-letter word. Try again...\n",
 	}, {
-		word:  "BEACH",
-		input: "beach",
+		word:   "BEACH",
+		inputs: []string{"beach"},
 		output: strings.Join([]string{
 			"\033[42m\033[1;30m B \033[m\033[m",
 			"\033[42m\033[1;30m E \033[m\033[m",
@@ -28,8 +28,8 @@ func TestRun(t *testing.T) {
 			"\n",
 		}, ""),
 	}, {
-		word:  "BEATS",
-		input: "burst",
+		word:   "BEATS",
+		inputs: []string{"burst"},
 		output: strings.Join([]string{
 			"\033[42m\033[1;30m B \033[m\033[m",
 			"\033[40m\033[1;37m U \033[m\033[m",
@@ -39,8 +39,8 @@ func TestRun(t *testing.T) {
 			"\n",
 		}, ""),
 	}, {
-		word:  "BOOTY",
-		input: "raise",
+		word:   "BOOTY",
+		inputs: []string{"raise"},
 		output: strings.Join([]string{
 			"\033[40m\033[1;37m R \033[m\033[m",
 			"\033[40m\033[1;37m A \033[m\033[m",
@@ -52,18 +52,21 @@ func TestRun(t *testing.T) {
 	}}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("when '%s' is provided as input", test.input), func(t *testing.T) {
+		t.Run(fmt.Sprintf("when '%s' is provided as input", strings.Join(test.inputs, ", ")), func(t *testing.T) {
 			var command, result bytes.Buffer
 			defer result.Reset()
 
-			fmt.Fprintf(&command, fmt.Sprintf("%s\n", test.input))
+			for _, input := range test.inputs {
+				fmt.Fprintf(&command, fmt.Sprintf("%s\n", input))
+			}
+
 			fmt.Fprintf(&command, "stop\n")
 
 			run(test.word, &command, &result)
 
 			got := result.String()
 			if !strings.Contains(got, test.output) {
-				t.Errorf("expected '%s' to produce output '%s'; got '%s'", test.input, test.output, got)
+				t.Errorf("expected '%s' to produce output '%s'; got '%s'", strings.Join(test.inputs, ","), test.output, got)
 			}
 		})
 	}
